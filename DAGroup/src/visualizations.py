d2dc504860ce493e7b31d33aadb2326f61cba95a
anderson <ander.cotrim@gmail.com>
2023-05-19 10:47:57 -0300

    add another notebook for data description

--- DAGroup/src/visualizations.py
+++ DAGroup/src/visualizations.py
@@ -28,8 +28,28 @@ def plot_pie(df, cod, title, desc_dict=None, ax=None, max_len=None):
     df = df[cod].value_counts()
     ax = df.plot(kind='pie', autopct='%1.1f%%', labels=None, legend=True, ax=ax)
     if desc_dict is not None:
-        ax.legend([desc_dict[v] for v in df.index.values])
+        ax.legend([desc_dict[v] for v in df.index.values], fontsize='small')
     ax.set_ylabel('Porcentagem')
+    ax.set_title(break_string(title, max_len))
+
+
+def plot_bar(df, cod, title, desc_dict=None, ax=None, max_len=None):
+    df = df[cod].value_counts(normalize=True) * 100
+    df = df.sort_index()
+    if desc_dict is not None:
+        df = df.rename(index=desc_dict)
+    ax = df.plot(kind='bar', ax=ax, width=0.8, legend=False)
+    ax.set_ylabel('Porcentagem')
+    # Set the x-axis tick labels at a diagonal angle
+    ax.set_xticklabels(df.index, rotation=45, ha='right')
+    ax.set_xlabel('')
+    ax.set_title(break_string(title, max_len))
+
+
+def plot_dist(data, title, ax=None, max_len=None):
+    sns.histplot(data, bins=20, kde=True, ax=ax, stat='percent')
+    ax.set_ylabel('Porcentagem')
+    ax.set_xlabel('')
     ax.set_title(break_string(title, max_len))
 
 
@@ -44,6 +64,8 @@ def plot_geopandas(df, uf_key, title=None):
 
     plt.show()
 
+    return fig
+
 
 def plot_correlation(df, x, y, title='', xlabel='x', ylabel='y'):
     jg = sns.jointplot(x=x, y=y, data=df, kind='reg')
@@ -57,3 +79,5 @@ def plot_correlation(df, x, y, title='', xlabel='x', ylabel='y'):
     phantom, = jg.ax_joint.plot([], [], linestyle="", alpha=0)
     jg.ax_joint.legend([phantom], [f"Correlation coefficient: {corr:.2f}\nP-value: {pval:.2e}"])
     plt.show()
+
+    return jg.fig
